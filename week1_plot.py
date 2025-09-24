06e8e9081a4610eefd559fea950fc50cfa960668
Natasha Babcock <natashaxbranding@gmail.com>
2025-09-24 19:50:49 -0400

    Pushing changes to week1_plot

--- week1_plot.py
+++ week1_plot.py
@@ -2,12 +2,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # TODO: load the dataset as pandas dataframe
+df_teeth = pd.read_csv("/Users/natashababcock/Documents/HMB491_modules/HMB491_week1/mammal_teeth.csv")
 
-plt.figure(figsize=(5, 10)) # set figure size
+plt.figure(figsize=(12, 15)) # set figure size
 plt.scatter(x=df_teeth['Top incisors'],
 y=df_teeth['MAMMAL']) # set figure x, y axis
-plt.gca().xaxis.set_visible(False)
+plt.gca().xaxis.set_visible(True)
 
 # TODO: change the title name to include your name
-plt.title("plot for mammal_teeth dataset")
-plt.savefig("mammal_teeth_scatterplot.png", dpi=150) # save the figure
+plt.title("Natasha's plot for mammal_teeth dataset")
+plt.savefig("natasha_mammal_teeth_scatterplot.png", dpi=150) # save the figure
